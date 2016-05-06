ccae38058c4995feac7cea83c23fb2696fc5863b
asistent <asistent0@gmail.com>
2016-05-06 21:06:20 +0300

    для проверки работы

---
+++ server2.js
@@ -0,0 +1,37 @@
+/**
+ * Created by asistent on 06.05.2016.
+ */
+
+var http = require('http');
+var request = require('request');
+var cheerio = require('cheerio');
+
+var PORT = 8000;
+var string = [];
+
+request('http://www.volzsky.ru/', function (error, response, html) {
+    if (error) {
+        return console.error('error is: ', error);
+    }
+
+    if (response.statusCode !== 200) {
+        return console.log('incorrect statusCode: ', response.statusCode);
+    }
+
+    var $ = cheerio.load(html);
+
+    $('a.morda').each(function (i, img) {
+                    string[i] = $(img).text().trim() + '<br>';
+    });
+});
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    response.write(string);
+    response.end();
+}).listen(PORT, function () {
+    console.log("Let's get started: ", PORT);
+});
