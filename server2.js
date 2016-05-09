d48e7fd33f71ceb614a9402cb62ed717dbe4f8ff
asistent <asistent0@gmail.com>
2016-05-09 22:57:56 +0300

    Настроил кодировку, получил данные с сайта

--- server2.js
+++ server2.js
@@ -5,11 +5,14 @@
 var http = require('http');
 var request = require('request');
 var cheerio = require('cheerio');
+var Iconv  = require('iconv').Iconv;
 
 var PORT = 8000;
 var string = [];
+var iconv = new Iconv('cp1251', 'utf-8');
 
-request('http://www.volzsky.ru/', function (error, response, html) {
+
+request({url: 'http://www.volzsky.ru/index.php?wx=16', encoding: null}, function (error, response, html) {
     if (error) {
         return console.error('error is: ', error);
     }
@@ -18,23 +21,32 @@ request('http://www.volzsky.ru/', function (error, response, html) {
         return console.log('incorrect statusCode: ', response.statusCode);
     }
 
+    html = iconv.convert(html).toString();
+
     var $ = cheerio.load(html);
 
-    $('a.morda').each(function (i, img) {
-                    string[i] = $(img).text().trim() + '<br>';
+    $('div.btc_block-1').each(function (i, element) {
+        $(element).find('div.btc_block-1_1.btc_h').children('a').attr('target','_blank');
+        $(element).find('div.btc_block-1_2').children('a').children('img').removeAttr('align');
 
+        string[i] = '<div>' + $(element).find('div.btc_block-1_1.btc_h').children('a') + '<br>' +
+            $(element).find('div.btc_block-1_2').children('a').children('img') + '<br>' +
+            $(element).find('div.btc_block-1_2').children('p.btc_p.btc_inf').html().split('|')[0] + '</div><br>';
     });
 
-    string = string.join('');
+
+    string = string.join('')
+        .replace(new RegExp("index.php",'g'), "http://www.volzsky.ru/index.php")
+        .replace(new RegExp("img/",'g'), "http://www.volzsky.ru/img/");
 });
 
 http.createServer(function (request, response) {
     response.writeHead(200, {
-        'Content-Type': 'text/html'
+        'Content-Type': 'text/html;  charset=utf-8'
     });
 
     response.write(string);
     response.end();
 }).listen(PORT, function () {
-    console.log("Let's get started: ", PORT);
+    console.log("Let's get started: http://localhost:" + PORT);
 });
