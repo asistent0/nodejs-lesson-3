58a1bf4aff1dcc756a5c48d80e3a868859c75b1a
asistent <asistent0@gmail.com>
2016-05-10 08:47:12 +0300

    Улучшил скрипт

--- translate.js
+++ translate.js
@@ -2,8 +2,6 @@
  * Created by asistent on 05.05.2016.
  */
 
-// TODO: API key: trnsl.1.1.20160505T191746Z.34dc4dd9c27b1897.e27acf0b35410b8882ecab6c36fc361b4c87a855
-
 var http = require('http');
 var fs = require('fs');
 var urlutils = require('url');
@@ -11,36 +9,40 @@ var request = require('request');
 
 var PORT = 8000;
 
-function onRequest(req, res) {
-    var parse = urlutils.parse(req.url, true);
+http.createServer(function (request, response) {
+
+    var parse = urlutils.parse(request.url, true);
     var page = parse.pathname;
     var text = parse.query.text;
 
     if (page === '/') {
         rend(fs.readFileSync('index.html'));
     }
-    if (page === '/ajax') {
+
+    if (page === '/trans') {
         translate(text, rend);
     }
 
     function rend(render) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(render);
-        res.end();
+        response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        response.write(render);
+        response.end();
     }
-}
+
+}).listen(PORT, function () {
+    console.log("Let's get started: http://localhost:" + PORT);
+});
 
 function translate(text, callback) {
     request.get('https://translate.yandex.net/api/v1.5/tr.json/translate?'
-        + 'key=trnsl.1.1.20160505T191746Z.34dc4dd9c27b1897.e27acf0b35410b8882ecab6c36fc361b4c87a855&lang=en-ru&text='
-        + text,
+        + 'key=trnsl.1.1.20160505T191746Z.34dc4dd9c27b1897.e27acf0b35410b8882ecab6c36fc361b4c87a855&'
+        + 'lang=en-ru&text=' + text,
         function (err, res, body) {
             if (err) {
                 callback(JSON.stringify({'status': 'error'}));
             } else {
                 if (res.statusCode === 200) {
                     var data = JSON.parse(body);
-                    console.log(data.text[0]);
                     callback(JSON.stringify({'status': 'success', 'msg': data.text[0]}));
                 } else {
                     callback(JSON.stringify({'status': 'error'}));
@@ -48,7 +50,3 @@ function translate(text, callback) {
             }
         })
 }
-
-http.createServer(onRequest).listen(PORT, function () {
-    console.log("Let's get started: http://localhost:" + PORT);
-});
